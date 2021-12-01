4e44cde040644c88ab425ecb7e77247cc6ab67dd
JimCode <jimmypaytan10@gmail.com>
2021-12-01 17:00:21 -0500

    capitulo 8

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::view('/users', 'users.showAll')->name('users.all');
 Route::view('/game', 'game.show')->name('game.show');
+
+Route::get('/chat', [ChatController::class, 'showChat'])->name('chat.show');
+Route::post('/chat/message', [ChatController::class, 'messageReceived'])->name('chat.message');
